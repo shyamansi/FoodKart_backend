46317ed7e8cfd5aa10784fc7ce67dea1308d15e7
Ubuntu <ubuntu@ip-172-31-4-199.ap-south-1.compute.internal>
2025-07-25 08:33:15 +0000

    updated code

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,15 +5,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
  private
  
  def respond_with(resource, options={})
- Rails.logger.debug"shyammmm from ruby #{resource.inspect} and #{resource.persisted?.inspect}"
+ Rails.logger.error"shyammmm from ruby #{resource.inspect} and #{resource.persisted?.inspect}"
  
  if resource.persisted?
-   Rails.logger.debug"shyammmm from ruby1"
+   Rails.logger.error"shyammmm from ruby1"
   render json: {
     status: {code: 201 ,message: "User created Successfully",data: resource}
   }, status: :ok
  else
-  Rails.logger.debug"shyammmm from ruby2"
+  Rails.logger.error"shyammmm from ruby2"
   render json:{
     status:{message:"User could not be created! Sorry",errors: resource.errors.full_messages},status: :unprocessable_entity
   }
